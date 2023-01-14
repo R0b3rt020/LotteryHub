a128f68731d3926716f505deaf30ff6228264f74
Roberto Meneses <63264150+R0b3rt020@users.noreply.github.com>
2023-01-14 14:52:32 +0000

    change to mainnet

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
 export const currency: string = "BNB";
-export const contractAddress: string = "0x6c8cCA21D5b5dD00FDcf1aD0c796FA2905F2BA7C";
+export const contractAddress: string = "0x21378B2c59c30e1DffDd4413612FBF9E5872E4c8";
